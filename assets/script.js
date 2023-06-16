93c1bdd34fbb453ee7ac6751e8ac14248727588e
Adeline34 <127093328+Adeline34@users.noreply.github.com>
2023-06-16 15:58:45 +0200

    essai bullet + img

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,62 @@
+//fléches
 const leftArrow = document.querySelector('.arrow_left');
 const rightArrow = document.querySelector('.arrow_right');
+const dots = document.querySelector('.dots');
+const img = document.querySelector('.banner-img');
+let positionSlide = 0;
+const txt = document.querySelector('.banner-txt');
 
-leftArrow.addEventListener('click',ClickOutCome)
 
-function ClickOutCome(){
-	alert('clique gauche')}
+//Addeventlistener
+leftArrow.addEventListener("click",()=>{
+	positionSlide--;
+	if (positionSlide === -1){
+		positionSlide = slides.lenght - 1;
+	}
+	  img.src = slides[positionSlide].image;
+	  txt.innerHTML = slides[positionSlide].tagLine;
+	  console.log("j'ai cliqué à gauche");
+	  severalDots();
+});
 
-rightArrow.addEventListener('click',Click)
+rightArrow.addEventListener("click",()=>{
+	positionSlide++;
+	if (positionSlide === slides.lenght){
+		positionSlide = 0;
+	}
+	  img.src = slides[positionSlide].image;
+	  txt.innerHTML = slides[positionSlide].tagLine;
+	  console.log("j'ai cliqué à droite");
+	  severalDots();
+});
 
-function Click(){
-	alert('clique droit')}
+for (let i= 0; i< slides.lenght;i++){
+	const newDot = document.createElement('div');
+	newDot.classList = 'dot';
+	dots.appendChild(newdot);
+	newDot.addEventListener('click',()=>{
+		positionSlide = i;
+		img.src = slides[positionSlide].image;
+		txt.innerHTML = slides[positionSlide].tagLine;
+		severalDots();
+	})
+}
+
+//Bullet point
+let dot = document.querySelectorAll('.dot');
+dot[0].classList.add('dot_selected');
+
+function severalDots () {
+	for (let i = 0; i < dot.length; i++) {
+		if (i === positionSlide) {
+			dot[i].classList.add('dot_selected');
+		} else {
+			dot[i].classList.remove('dot_selected');
+		}
+	}
+}
+
+
 
 const slides = [
 	{
@@ -29,3 +76,4 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
